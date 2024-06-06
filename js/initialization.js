69a3c8b614ab517066c08f00b63997b9c294e676
shibadisaster <38396090+shibadisaster@users.noreply.github.com>
2024-06-07 07:40:34 +0800

    Improve header and add flex

--- js/initialization.js
+++ js/initialization.js
@@ -4,8 +4,12 @@ ants.amount = new Decimal(2);
 
 
 
+
+
 // value declarations
 carrying_capacity = new Value("ant_carrying_capacity", new Decimal(50));
+
+
 
 
 
@@ -21,7 +25,7 @@ ants_over_carrying_capacity = new ValueModifier("Over Carrying Capacity", "MULT"
 ants_over_carrying_capacity.value_function = function() {
     var surplus = game.resources.ants.amount.sub(game.values.ant_carrying_capacity.value);
     if (surplus.lte(0)) {
-        this.value = new Decimal(1);
+        this.value = new Decimal('1');
     }
     else {
         this.value = new Decimal('0.95').pow(surplus);
@@ -30,8 +34,15 @@ ants_over_carrying_capacity.value_function = function() {
 
 
 
+
+
 // lock declarations
 lock_50_ants = new Lock("50_ants", true);
 lock_50_ants.unlock_criteria = function() {
-    return game.resources.ants.amount.gte("50") ? true : false;
+    return game.resources.ants.amount.gte('50') ? true : false;
+}
+
+lock_80_ants = new Lock("80_ants", true);
+lock_80_ants.unlock_criteria = function() {
+    return game.resources.ants.amount.gte('80') ? true : false;
 }
